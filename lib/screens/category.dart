f1eed1ab2f40eca8119fa1123a01734b76abce4c
salma hamdy <74612024+salmaHamdy2@users.noreply.github.com>
2023-07-17 05:28:22 +0300

    salma

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -17,7 +17,7 @@ class categoryscreen extends StatelessWidget {
        Image.network("https://www.totalsportal.com/wp-content/uploads/2021/12/Sports-Pictures-1200x715.jpg")
        ,qfun:(){    Navigator.pushAndRemoveUntil(
                     context,
-                    MaterialPageRoute(builder: (context) => const ques()),
+                    MaterialPageRoute(builder: (context) =>  ques(themeColor: Colors.blue,cname: "sport questions",)),
                     (route) => false,
                   );} ,),
       Padding(padding: EdgeInsets.all(6)),
@@ -25,7 +25,7 @@ class categoryscreen extends StatelessWidget {
       categorycontainer(img: Image.network("https://edurev.gumlet.io/ApplicationImages/Temp/13879414_71b19982-d0f4-4260-bd70-c257590d3598_lg.png?w=400&dpr=2.6")
       ,qfun:(){    Navigator.pushAndRemoveUntil(
                     context,
-                    MaterialPageRoute(builder: (context) => const ques()),
+                    MaterialPageRoute(builder: (context) =>  ques(themeColor: Colors.amber,cname: "math questions",)),
                     (route) => false,
                   );} ,),
        
@@ -34,7 +34,7 @@ class categoryscreen extends StatelessWidget {
        categorycontainer(img: Image.network("https://www.eoa.org.eg/2022/general-info/General-info.png")
        ,qfun:(){    Navigator.pushAndRemoveUntil(
                     context,
-                    MaterialPageRoute(builder: (context) => const ques()),
+                    MaterialPageRoute(builder: (context) => ques(themeColor: Colors.red,cname: "general questions",)),
                     (route) => false,
                   );}),
       
--- lib/screens/ques.dart
+++ lib/screens/ques.dart
@@ -2,64 +2,83 @@ import 'package:flutter/material.dart';
 import 'package:quiz/screens/home.dart';
 import 'package:quiz/screens/result.dart';
 
-
 class ques extends StatelessWidget {
-  const ques({super.key});
-
+   ques({super.key,this.themeColor,this.cname});
+  final Color? themeColor;
+  final String? cname;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(automaticallyImplyLeading: false,
-
-leading: Center(child: Text("sport questions")),
-leadingWidth: 100,
-title:Column(children: [Text('Question no:'), Text("4/20"),],),
-centerTitle: true,
-actions: [Padding(padding: EdgeInsets.all(10),child: Icon(Icons.sports_baseball_rounded))],
-
-
-
-),
-body: Container(
-  width: double.infinity,
-child: Column(
-  crossAxisAlignment:CrossAxisAlignment.center,
-
-children: [
-  SizedBox(height: 20,),
-Text("Question:"),
-Text(":كم عدد اللاعبين في فريق كره القدم"),
-SizedBox(height: 20,),
-Text("answer:"),
-ElevatedButton(onPressed: (){   Navigator.pushAndRemoveUntil(
+      appBar: AppBar(backgroundColor: themeColor,
+        automaticallyImplyLeading: false,
+        leading: Center(child: Text(cname!)),
+        leadingWidth: 120,
+        title: Column(
+          children: [
+            Text('Question no:'),
+            Text("4/20"),
+          ],
+        ),
+        centerTitle: true,
+        actions: [
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: Icon(Icons.sports_baseball_rounded))
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text("Question:"),
+            Text(":كم عدد اللاعبين في فريق كره القدم"),
+            SizedBox(
+              height: 20,
+            ),
+            Text("answer:"),
+            ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: themeColor),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(builder: (context) => const score()),
                     (route) => false,
-                  );}, child: Text("10")),
-ElevatedButton(onPressed: (){   Navigator.pushAndRemoveUntil(
+                  );
+                },
+                child: Text("10")),
+            ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: themeColor),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(builder: (context) => const score()),
                     (route) => false,
-                  );}, child: Text("11")),
-ElevatedButton(onPressed: (){   Navigator.pushAndRemoveUntil(
+                  );
+                },
+                child: Text("11")),
+            ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: themeColor),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(builder: (context) => const score()),
                     (route) => false,
-                  );}, child: Text("12")),
-ElevatedButton(onPressed: (){   Navigator.pushAndRemoveUntil(
+                  );
+                },
+                child: Text("12")),
+            ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: themeColor),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(builder: (context) => const score()),
                     (route) => false,
-                  );}, child: Text("15")),
-
-],
-
-
-),
-
-
-),
-
+                  );
+                },
+                child: Text("15")),
+          ],
+        ),
+      ),
     );
   }
 }
